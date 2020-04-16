b4fec79fcecad6863cfaba1151a4bd3e46980513
Ilia Moskalenko <i.moskalenkoxvx@gmail.com>
2020-04-16 20:13:06 +0300

    Reformat code

--- GeekBrainsProject/src/HomeWorkFive/AnimalTest.java
+++ GeekBrainsProject/src/HomeWorkFive/AnimalTest.java
@@ -4,9 +4,9 @@ package HomeWorkFive;
 public class AnimalTest {
     public static void main(String[] args) {
 
-        Bowl bowl = new Bowl(20,4);
-        System.out.println("Количество еды в миске "+
-                + bowl.getFoodMas() + " Миска полная: " + bowl.isFull());
+        Bowl bowl = new Bowl(20, 4);
+        System.out.println("Количество еды в миске " +
+                +bowl.getFoodMas() + " Миска полная: " + bowl.isFull());
 
         Cat[] cats = new Cat[5];
         cats[0] = new Cat("Барсик", 4);
@@ -15,7 +15,7 @@ public class AnimalTest {
         cats[3] = new Cat("Мурка", 5);
         cats[4] = new Cat("Петр", 4);
 
-        for (Cat cat : cats){
+        for (Cat cat : cats) {
             cat.eat(bowl);
             if (!cat.isFull()) cat.eat(bowl);
             bowl.info();
